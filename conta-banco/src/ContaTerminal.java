2356e00abfe264b8ec0dd108b8be87fe638bfe03
SHALOM ACACIO DOS SANTOS <shalom.santos@tce.ce.gov.br>
2024-05-02 23:18:06 -0300

    metodo para criar conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usario 
+        //Exibir a mensagem da conta criada
+
+        Scanner resposta = new Scanner(System.in);
+
+        System.out.println("\n" +"Digite o numero de sua agencia");
+        String agencia = resposta.next();
+
+        System.out.println("Digite o numero de sua conta");
+        int numero = resposta.nextInt();
+
+        System.out.println("Digite seu nome");
+        String nomeCliente = resposta.next();
+
+        System.out.println("Digite o saldo");
+        double saldoInicial = 0.00D;
+
+        System.out.println("Olá "+ nomeCliente 
+        +", obrigado por criar uma conta em nosso banco," +"\n"
+        +" sua agência é : " + agencia +"\n"
+        +" Conta: " + numero +"\n"
+        +" Saldo Inicial: " + saldoInicial );
+
+        resposta.close();    
     }
 }
